e9bed7b15095d099a7501ef67c0eaee83a885ebd
ErRickow <remixingmagelang@gmail.com>
2025-08-23 00:08:26 +0700

    fix: removing unused import

--- app/layout-client.tsx
+++ app/layout-client.tsx
@@ -1,13 +1,12 @@
 "use client"
 
-import { API_ROUTE_CSRF } from "@/lib/routes"
 import { useQuery } from "@tanstack/react-query"
 
 export function LayoutClient() {
   useQuery({
     queryKey: ["csrf-init"],
     queryFn: async () => {
-      await fetch(API_ROUTE_CSRF)
+      await fetch("api/csrf")
       return true
     },
     staleTime: Infinity,
